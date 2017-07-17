de7049a9ffef1cdbc6b9a68af21fce1fdffd9a0f
Nidylei <v-feile@microsoft.com>
2017-07-17 11:05:02 +0800

    switch to debug model

--- remote-scripts/create-sql-rg.sh
+++ remote-scripts/create-sql-rg.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-trap 'echo fail to run script;exit 1' ERR
+trap "echo fail to run script;exit 1" ERR
 apt-get update
 apt-get install -y zip
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
@@ -12,8 +12,9 @@ sudo apt-get install apt-transport-https
 sudo apt-get update && sudo apt-get install azure-cli
 
 apt-get install -y git
+rm meta-azure-service-broker -rf
 git clone https://github.com/Azure/meta-azure-service-broker
-
+sed -i 's/info/debug/' meta-azure-service-broker/winston.json
 az login --service-principal --tenant REPLACE_WITH_TENANTID --username REPLACE_WITH_CLIENTID --password REPLACE_WITH_CLIENTSECRET
 export resourcegroupname="REPLACE_WITH_RESOURCEGROUPNAME"
 export location="REPLACE_WITH_LOCATION"
